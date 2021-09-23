cd67909b915be389de2f2aaf69979a2f82eb962c
Randriamanantsoa Léa <leamanantsoa.dev@gmail.com>
2021-09-23 19:15:48 +0100

    change title

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,15 +13,15 @@
 import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/CompositionComponent.vue';
 import { defineComponent, ref } from 'vue';
-import { useMeta } from 'quasar';
+// import { useMeta } from 'quasar';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { ExampleComponent },
-  setup() {
-    useMeta({
-      title: 'Learncasts'
-    })
+  // components: { ExampleComponent },
+  // setup() {
+  //   useMeta({
+  //     title: 'Learncasts'
+  //   })
     const todos = ref<Todo[]>([
       {
         id: 1,
