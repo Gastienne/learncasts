000e2016490c9255c5db55a51fef84fb842a8884
Lea <leamanantsoa.dev@gmail.com>
2021-09-28 19:09:14 +0300

    Add return

--- netlify/functions/deploy-succeeded.ts
+++ netlify/functions/deploy-succeeded.ts
@@ -1,11 +1,12 @@
 import { Handler } from '@netlify/functions';
 
 const handler: Handler = () => {
-  console.log(process.env.DEPLOY_PRIME_URL)
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({ message: 'Hello World' }),
-  // };
+  console.log(process.env)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello World' }),
+  };
 };
 
 export { handler };
