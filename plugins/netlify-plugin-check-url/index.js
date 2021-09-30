65a793565d20e1a398367bd86bb8e276e67a902d
Lea <leamanantsoa.dev@gmail.com>
2021-09-30 15:10:56 +0300

    async await

--- plugins/netlify-plugin-check-url/index.js
+++ plugins/netlify-plugin-check-url/index.js
@@ -1,37 +1,39 @@
 const axios = require('axios')
-// import fetch from 'node-fetch';
+const siteName = process.env.SITE_NAME
+const deployId = process.env.DEPLOY_ID
+const idPR = process.env.REVIEW_ID
+const branch = process.env.HEAD
+const repo = process.env.REPOSITORY_URL
 
-// const siteName = process.env.SITE_NAME
-// const deployId = process.env.DEPLOY_ID
-// const idPR = process.env.REVIEW_ID
-// const branch = process.env.HEAD
-// const repo = process.env.REPOSITORY_URL
+async function sendDeployStatus(isSuccess = false) {
+  const url = `https://app.netlify.com/sites/${siteName}/deploys/${deployId}`
+  const data = {
+    'onSuccess': isSuccess,
+    'url': url,
+    'idPR': idPR,
+    'branchName': branch,
+    'repo': repo
+  }
+
+  await axios.post('/api/urlDeployPreview', data)
+  .then(function (response) {
+    console.log(response)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+}
 
 /* eslint-disable no-unused-vars */
 module.exports = {
-  onSuccess: (() => {
-    console.log('OnSuccess');
-    if (!process.env.PULL_REQUEST){
-      return;
-    }
-    const apiUrl = 'https://api.my-ip.io/ip.json'
-      axios({
-        method: 'get',
-        url: apiUrl,
-        responseType: 'stream'
-      }).then(function (response) {
-        // handle success
-        console.log(' ##################### log ###########', response);
-      })
-      .then(function (error) {
-        // handle error
-        console.log(' ##################### log ###########', error);
-      })
-    console.log('eto ambany');
-  }),
-  onError: (() => {
-    if (!process.env.PULL_REQUEST)
-      return;
-    console.log('OnError');
-  })
+  onSuccess() {
+    if (!process.env.PULL_REQUEST) return;
+    console.log('OnSuccess')
+    sendDeployStatus(true)
+  },
+  onError() {
+    if (!process.env.PULL_REQUEST) return;
+    console.log('OnError')
+    sendDeployStatus()
+  }
 }
