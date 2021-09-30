6aea280ef96772482ec725603e3e074abb4d9567
Lea <leamanantsoa.dev@gmail.com>
2021-09-30 12:13:28 +0300

    console log hita b

--- plugins/netlify-plugin-check-url/index.js
+++ plugins/netlify-plugin-check-url/index.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+// const axios = require('axios');
 const siteName = process.env.SITE_NAME
 const deployId = process.env.DEPLOY_ID
 const idPR = process.env.REVIEW_ID
@@ -6,39 +6,31 @@ const branch = process.env.HEAD
 const repo = process.env.REPOSITORY_URL
 
 function sendDeployStatus(isSuccess = false) {
-  const url = `https://app.netlify.com/sites/${siteName}/deploys/${deployId}`
-  const data = {
-    'onSuccess': isSuccess,
-    'url': url,
-    'idPR': idPR,
-    'branchName': branch,
-    'repo': repo
-  }
+  console.log('VOAHANTSO')
+  fetch('https://api.my-ip.io/ip.json')
+  .then(resp =>  console.log('########## HITA BE ######### ', resp)).then(err => console.log('########### error ###########', err))
+  // const url = `https://app.netlify.com/sites/${siteName}/deploys/${deployId}`
+  // const data = {
+  //   'onSuccess': isSuccess,
+  //   'url': url,
+  //   'idPR': idPR,
+  //   'branchName': branch,
+  //   'repo': repo
+  // }
 
-  const apiUrl = 'https://poc-gestion-projet-db9a07.netlify.live/.netlify/functions/hello'
-  // axios({
-  //   method: 'POST',
-  //   apiUrl,
-  //   data
-  // })
-  // .then(function (response) {
-  //   console.log('ok', response)
-  // })
-  // .catch(function (error) {
-  //   console.log('error', error)
-  // })
-  axios.get(apiUrl)
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+  // const apiUrl = 'https://poc-gestion-projet-db9a07.netlify.live/.netlify/functions/hello'
+  // // axios({
+  // //   method: 'POST',
+  // //   apiUrl,
+  // //   data
+  // // })
+  // // .then(function (response) {
+  // //   console.log('ok', response)
+  // // })
+  // // .catch(function (error) {
+  // //   console.log('error', error)
+  // // })
+
 }
 /* eslint-disable no-unused-vars */
 module.exports = {
