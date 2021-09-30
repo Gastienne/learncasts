4f9289514da9c08114886c56dbbc7cb87f26719a
Lea <leamanantsoa.dev@gmail.com>
2021-09-30 16:20:50 +0300

    check test onSuccess

--- test/cypress/integration/home.spec.ts
+++ test/cypress/integration/home.spec.ts
@@ -10,7 +10,7 @@ describe('Landing', () => {
     cy.testRoute('/')
   });
   it('.should() - assert that <title> is correct', () => {
-    cy.title().should('include', 'Test');
+    cy.title().should('include', 'Learncasts');
   });
 });
 
