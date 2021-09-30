94122541786bf956868b9485e845a3a80fe91e12
Lea <leamanantsoa.dev@gmail.com>
2021-09-30 14:53:12 +0300

    test eto

--- plugins/netlify-plugin-check-url/index.js
+++ plugins/netlify-plugin-check-url/index.js
@@ -7,38 +7,25 @@ const axios = require('axios')
 // const branch = process.env.HEAD
 // const repo = process.env.REPOSITORY_URL
 
-function sendDeployStatus(isSuccess = false) {
-  console.log('VOAHANTSO')
-  // const url = `https://app.netlify.com/sites/${siteName}/deploys/${deployId}`
-  // const data = {
-  //   'onSuccess': isSuccess,
-  //   'url': url,
-  //   'idPR': idPR,
-  //   'branchName': branch,
-  //   'repo': repo
-  // }
-
-  const apiUrl = 'https://poc-gestion-projet-db9a07.netlify.live/.netlify/functions/hello'
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  axios({
-    method: 'get',
-    url: apiUrl,
-    responseType: 'stream'
-  }).then(function (response) {
-    // handle success
-    console.log(' ##################### log ###########', response);
-  })
-  .then(function (error) {
-    // handle error
-    console.log(' ##################### log ###########', error);
-  })
-
-}
 /* eslint-disable no-unused-vars */
 module.exports = {
   onSuccess: (() => {
-    if (!process.env.PULL_REQUEST)
+    if (!process.env.PULL_REQUEST){
       return;
+    }
+    const apiUrl = 'https://poc-gestion-projet-db9a07.netlify.live/.netlify/functions/hello'
+      axios({
+        method: 'get',
+        url: apiUrl,
+        responseType: 'stream'
+      }).then(function (response) {
+        // handle success
+        console.log(' ##################### log ###########', response);
+      })
+      .then(function (error) {
+        // handle error
+        console.log(' ##################### log ###########', error);
+      })
     console.log('OnSuccess');
     sendDeployStatus(true);
     console.log('eto ambany');
@@ -47,6 +34,5 @@ module.exports = {
     if (!process.env.PULL_REQUEST)
       return;
     console.log('OnError');
-    sendDeployStatus();
   })
 }
